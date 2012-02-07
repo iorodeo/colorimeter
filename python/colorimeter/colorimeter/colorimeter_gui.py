72424183e9b80c95f8fba276cc18d565c5811b04
Will Dickson <will@iorodeo.com>
2012-02-07 12:00:49 -0800

    Fixed bug in firmware which caused white values to be the same as the red values. Added color selection checkboxes to GUI. Added number of samples line edit to GUI.

--- python/colorimeter/colorimeter/colorimeter_gui.py
+++ python/colorimeter/colorimeter/colorimeter_gui.py
@@ -22,12 +22,16 @@ class ColorimeterMainWindow(QtGui.QMainWindow,Ui_MainWindow):
         self.calibratePushButton.clicked.connect(self.calibrateClicked_Callback)
         self.measurePushButton.clicked.connect(self.measureClicked_Callback)
         self.measurePushButton.pressed.connect(self.measurePressed_Callback)
+        self.samplesLineEdit.editingFinished.connect(self.samplesChanged_Callback)
 
     def initialize(self):
         self.port = DFLT_PORT
         self.portLineEdit.setText(self.port) 
         self.setWidgetEnableOnDisconnect()
         self.dev = None
+        self.numSamples = None
+        self.samplesValidator = QtGui.QIntValidator(0,2**16-1,self.samplesLineEdit)
+        self.samplesLineEdit.setValidator(self.samplesValidator)
 
     def portChanged_Callback(self):
         self.port = str(self.portLineEdit.text())
@@ -37,16 +41,18 @@ class ColorimeterMainWindow(QtGui.QMainWindow,Ui_MainWindow):
             self.connectPushButton.setText('Disconnect')
             self.portLineEdit.setEnabled(False)
 
-
     def connectClicked_Callback(self):
         if self.dev == None:
             self.dev = Colorimeter(self.port)
             self.setWidgetEnableOnConnect()
+            self.numSamples = self.dev.getNumSamples()
+            self.samplesLineEdit.setText('{0}'.format(self.numSamples))
         else:
             self.connectPushButton.setText('Connect')
             self.dev.close()
             self.dev = None
             self.setWidgetEnableOnDisconnect()
+            self.samplesLineEdit.setText('')
 
     def calibratePressed_Callback(self):
         self.transmissionTextEdit.setText('')
@@ -56,6 +62,11 @@ class ColorimeterMainWindow(QtGui.QMainWindow,Ui_MainWindow):
         self.transmissionGroupBox.setEnabled(False)
         self.absorbanceGroupBox.setEnabled(False)
         self.measurePushButton.setEnabled(False)
+        self.samplesLineEdit.setEnabled(False)
+        self.redCheckBox.setEnabled(False)
+        self.greenCheckBox.setEnabled(False)
+        self.blueCheckBox.setEnabled(False)
+        self.whiteCheckBox.setEnabled(False)
 
     def calibrateClicked_Callback(self):
         self.dev.calibrate()
@@ -69,27 +80,40 @@ class ColorimeterMainWindow(QtGui.QMainWindow,Ui_MainWindow):
         self.transmissionGroupBox.setEnabled(False)
         self.absorbanceGroupBox.setEnabled(False)
         self.calibratePushButton.setEnabled(False)
+        self.samplesLineEdit.setEnabled(False)
+        self.redCheckBox.setEnabled(False)
+        self.greenCheckBox.setEnabled(False)
+        self.blueCheckBox.setEnabled(False)
+        self.whiteCheckBox.setEnabled(False)
 
     def measureClicked_Callback(self):
         freq, trans, absorb = self.dev.getMeasurement()
-        transStrList = [
-                'red:    {0:1.2f}'.format(trans[0]),
-                'green:  {0:1.2f}'.format(trans[1]),
-                'blue:   {0:1.2f}'.format(trans[2]),
-                'white:  {0:1.2f}'.format(trans[3]),
-                ]
+        transStrList = []
+        absorbStrList = []
+        if self.redCheckBox.isChecked():
+            transStrList.append('red:    {0:1.2f}'.format(trans[0]))
+            absorbStrList.append('red:    {0:1.2f}'.format(absorb[0]))
+        if self.greenCheckBox.isChecked():
+            transStrList.append('green:  {0:1.2f}'.format(trans[1]))
+            absorbStrList.append('green:  {0:1.2f}'.format(absorb[1]))
+        if self.blueCheckBox.isChecked():
+            transStrList.append('blue:   {0:1.2f}'.format(trans[2]))
+            absorbStrList.append('blue:   {0:1.2f}'.format(absorb[2]))
+        if self.whiteCheckBox.isChecked():
+            transStrList.append('white:  {0:1.2f}'.format(trans[3]))
+            absorbStrList.append('white:  {0:1.2f}'.format(absorb[3]))
         transStr = '\n'.join(transStrList)
-        self.transmissionTextEdit.setText(transStr)
-                
-        absorbStrList = [ 
-                'red:    {0:1.2f}'.format(absorb[0]),
-                'green:  {0:1.2f}'.format(absorb[1]),
-                'blue:   {0:1.2f}'.format(absorb[2]),
-                'white:  {0:1.2f}'.format(absorb[3]),
-                ]
         absorbStr = '\n'.join(absorbStrList)
+        self.transmissionTextEdit.setText(transStr)
         self.absorbanceTextEdit.setText(absorbStr)
         self.setWidgetEnableOnConnect()
+
+    def samplesChanged_Callback(self):
+        valueStr = str(self.samplesLineEdit.text())
+        value = int(valueStr)
+        if value != self.numSamples:
+            self.numSamples = value
+            self.dev.setNumSamples(value)
 
     def setWidgetEnableOnConnect(self):
         self.transmissionTextEdit.setEnabled(True)
@@ -98,6 +122,11 @@ class ColorimeterMainWindow(QtGui.QMainWindow,Ui_MainWindow):
         self.measurePushButton.setEnabled(True)
         self.transmissionGroupBox.setEnabled(True)
         self.absorbanceGroupBox.setEnabled(True)
+        self.samplesLineEdit.setEnabled(True)
+        self.redCheckBox.setEnabled(True)
+        self.greenCheckBox.setEnabled(True)
+        self.blueCheckBox.setEnabled(True)
+        self.whiteCheckBox.setEnabled(True)
         self.portLineEdit.setEnabled(False)
 
     def setWidgetEnableOnDisconnect(self):
@@ -107,6 +136,11 @@ class ColorimeterMainWindow(QtGui.QMainWindow,Ui_MainWindow):
         self.measurePushButton.setEnabled(False)
         self.transmissionGroupBox.setEnabled(False)
         self.absorbanceGroupBox.setEnabled(False)
+        self.samplesLineEdit.setEnabled(False)
+        self.redCheckBox.setEnabled(False)
+        self.greenCheckBox.setEnabled(False)
+        self.blueCheckBox.setEnabled(False)
+        self.whiteCheckBox.setEnabled(False)
         self.portLineEdit.setEnabled(True)
 
     def main(self):

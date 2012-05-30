099690be79c890d88680b97d1ab3cb20eb7eb3e6
Jo Long <jo@iorodeo.com>
2012-05-30 12:21:51 -0700

    Modified inner panel slot position so that it is better aligned with the led and sensor.

--- enclosure/make_enclosure.py
+++ enclosure/make_enclosure.py
@@ -7,9 +7,11 @@ from colorimeter_enclosure_v3 import Colorimeter_Enclosure
 INCH2MM = 25.4
 
 # Inside dimensions
-x,y,z = 2.5*INCH2MM, 3.0*INCH2MM, 1.75*INCH2MM
+#x,y,z = 2.5*INCH2MM, 3.0*INCH2MM, 1.75*INCH2MM
+x,y,z = 2.61*INCH2MM, 3.0*INCH2MM, 1.75*INCH2MM
 wall_thickness = 3.0 
 cuvette_size = 12.35,12.35
+cuvette_offset = -0.11*INCH2MM
 
 hole_list = []
 
@@ -18,10 +20,14 @@ params = {
         'inner_dimensions'        : (x,y,z), 
         'wall_thickness'          : wall_thickness, 
         'lid_radius'              : 0.25*INCH2MM,  
-        'top_x_overhang'          : 0.2*INCH2MM,
-        'top_y_overhang'          : 0.2*INCH2MM,
-        'bottom_x_overhang'       : 0.2*INCH2MM,
-        'bottom_y_overhang'       : 0.2*INCH2MM, 
+        #'top_x_overhang'          : 0.2*INCH2MM,
+        #'top_y_overhang'          : 0.2*INCH2MM,
+        #'bottom_x_overhang'       : 0.2*INCH2MM,
+        #'bottom_y_overhang'       : 0.2*INCH2MM, 
+        'top_x_overhang'          : 0.16*INCH2MM,
+        'top_y_overhang'          : 0.16*INCH2MM,
+        'bottom_x_overhang'       : 0.16*INCH2MM,
+        'bottom_y_overhang'       : 0.16*INCH2MM, 
         'lid2front_tabs'          : (0.25,0.75),
         'lid2side_tabs'           : (0.25, 0.75),
         'side2side_tabs'          : (0.5,),
@@ -35,11 +41,13 @@ params = {
         'standoff_diameter'       : 0.25*INCH2MM,
         'standoff_offset'         : 0.05*INCH2MM,
         'standoff_hole_diameter'  : 0.116*INCH2MM, 
-        'inner_panel_offset'      : 0.5*cuvette_size[0] + 0.5*wall_thickness,
+        #'inner_panel_offset'      : 0.5*cuvette_size[0] + 0.5*wall_thickness,
+        'inner_panel_offset'      : 0.5*cuvette_size[0] + 0.5*wall_thickness + cuvette_offset,
         'led_cable_hole_position' : (-1.0*INCH2MM, -0.45*INCH2MM),
         'led_cable_hole_size'     : 3.0,
         'cuvette_slot_size'       : (3.0,5.0),
-        'cuvette_slot_position'   : (0.0, -6.55),
+        #'cuvette_slot_position'   : (0.0, -6.55),
+        'cuvette_slot_position'   : (0.0, -0.15*INCH2MM),
         'pcb_position'            : (0,-0.15*INCH2MM),
         'pcb_hole_spacing'        : (1.6*INCH2MM, 0.8*INCH2MM),
         'pcb_mount_hole_diam'     : 0.12* INCH2MM,
@@ -65,8 +73,8 @@ enclosure.make()
 enclosure.add_holes(hole_list)
 
 part_assembly = enclosure.get_assembly(
-        explode=(0,0,0),
-        show_top=True,
+        explode=(0,0,5),
+        show_top=False,
         show_bottom=True,
         show_left=True,
         show_right=True,
@@ -76,9 +84,9 @@ part_assembly = enclosure.get_assembly(
         show_standoffs=True,
         show_holder_standoffs=True,
         show_inner_panel=True,
-        show_second_top=True,
-        show_outer_slider=True,
-        show_slider=True,
+        show_second_top=False,
+        show_outer_slider=False,
+        show_slider=False,
         )
 
 #part_projection = enclosure.get_projection(project=True)

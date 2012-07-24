1f61bb0bcf4fa2a2052f14b6e47e7ddfaffeac57
Will Dickson <will@iorodeo.com>
2012-07-24 13:44:15 -0700

    Started rearranging directory structure.

---
+++ python/Colorimeter/colorimeter/__init__.py
@@ -0,0 +1,6 @@
+from colorimeter_serial import *
+import main_window
+import table_widget
+import standard_curve
+import constants
+import import_export
--- python/Colorimeter/colorimeter_serial/__init__.py
+++
@@ -1,1 +0,0 @@
-from colorimeter_serial import *
